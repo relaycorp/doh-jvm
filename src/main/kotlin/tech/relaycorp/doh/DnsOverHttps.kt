44cc2b3d5131085ef10b5acda5e9fa4979c2c3f2
Gus Narea <gnarea@users.noreply.github.com>
2020-12-26 22:24:51 +0000

    constructor

--- src/main/kotlin/tech/relaycorp/doh/DnsOverHttps.kt
+++ src/main/kotlin/tech/relaycorp/doh/DnsOverHttps.kt
@@ -1,3 +1,7 @@
 package tech.relaycorp.doh
 
-public class DnsOverHttps
+public class DnsOverHttps(public val resolverURL: String = DEFAULT_RESOLVER_URL) {
+    public companion object {
+        public const val DEFAULT_RESOLVER_URL: String = "https://cloudflare-dns.com/dns-query"
+    }
+}
--- src/test/kotlin/tech/relaycorp/doh/DnsOverHttpsTest.kt
+++ src/test/kotlin/tech/relaycorp/doh/DnsOverHttpsTest.kt
@@ -1,3 +1,59 @@
 package tech.relaycorp.doh
 
-class DnsOverHttpsTest
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class DnsOverHttpsTest {
+    @Nested
+    inner class Constructor {
+        @Test
+        fun `CloudFlare DoH resolver should be used by default`() {
+            val doh = DnsOverHttps()
+
+            assertEquals("https://cloudflare-dns.com/dns-query", doh.resolverURL)
+        }
+
+        @Test
+        fun `Custom resolver URL should be allowed`() {
+            val customResolverURL = "https://example.com/dns-query"
+            val doh = DnsOverHttps(customResolverURL)
+
+            assertEquals(customResolverURL, doh.resolverURL)
+        }
+
+        @Test
+        @Disabled
+        fun `Default KTor client should be used by default`() {
+        }
+
+        @Test
+        @Disabled
+        fun `Custom KTor client can be specified`() {
+        }
+    }
+
+    @Nested
+    inner class Lookup {
+        @Test
+        @Disabled
+        fun `Specified domain name should be looked up`() {
+        }
+
+        @Test
+        @Disabled
+        fun `Specified record type should be looked up`() {
+        }
+
+        @Test
+        @Disabled
+        fun `DNSSEC verification should be requested by default`() {
+        }
+
+        @Test
+        @Disabled
+        fun `DNSSEC can be disabled if requested`() {
+        }
+    }
+}
